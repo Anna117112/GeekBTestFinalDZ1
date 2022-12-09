7877a564b179f75230626144b4aae91bb7c8c990
Anna <a.dyakonu85@mail.ru>
2022-12-09 11:29:04 +0300

    ввод массива

---
+++ dz/Program.cs
@@ -0,0 +1,17 @@
+﻿String[] UserArr()
+{
+
+    System.Console.WriteLine("Введите слова через пробед");
+    String s = Console.ReadLine();
+    if (s.Length != 0)
+    {
+        String[] arr = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        return arr;
+    }
+    else
+    {
+        String[] arr = new String[0];
+        return arr;
+    }
+}
+
